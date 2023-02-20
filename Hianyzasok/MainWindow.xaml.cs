a7dc30dadd76a5a45003e707c64149af36fc53ea
Attirobert <karobatti@gmail.com>
2023-02-20 20:18:58 +0100

    Hiányzások

--- Hianyzasok/MainWindow.xaml.cs
+++ Hianyzasok/MainWindow.xaml.cs
@@ -37,12 +37,13 @@ namespace Hianyzasok
                 M_Orak;
         }
 
-        string file = @"..\..\szeptember.csv";
+        string file = @"..\..\..\szeptember.csv";
+
+        // Diákok eltárolására
+        List<hianyzok> adatok = new List<hianyzok>();
 
         private void btnRead_Click(object sender, RoutedEventArgs e)
         {
-            // Diákok eltárolására
-            List<hianyzok> adatok = new List<hianyzok>();
             hianyzok adat = new hianyzok(); // 
 
             string[] darabol;
@@ -74,12 +75,82 @@ namespace Hianyzasok
             lblFeladat2.Content += "\n\tÖsszes mulasztott órák száma:" + adatok.Sum(x => x.M_Orak) + " óra.";
         }
 
+        // 4. feladat
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
             int mNap = Convert.ToInt32(tbxNap.Text);
             string mNev = tbxNev.Text;
 
+            // Mezők alaphelyzetbe állítása
+            tbxNap.Clear();
+            tbxNev.Clear();
 
+            if (adatok.Count(x => x.Nev == mNev) > 0)
+            {
+                lblFeladat4.Content = "4. feladat\n\tA tanuló hiányzott szeptemberben";
+            }
+            else
+            {
+                lblFeladat4.Content = "4. feladat\n\tA tanuló nem hiányzott szeptemberben";
+            }
+
+        }
+
+        // 3. feladat - adat ellenőrzés
+        private void tbxNap_LostFocus(object sender, RoutedEventArgs e)
+        {
+            int nap = 0;
+            try
+            {
+                nap = Convert.ToInt32(tbxNap.Text);
+                if (nap < 1 || nap > 30)
+                {
+                    MessageBox.Show("A hónap napjának [1, 30] tartományba kell esnie!");
+                    tbxNap.Text = "1";
+                    tbxNap.Focus();
+                }
+            }
+            catch (FormatException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnFeladat5_Click(object sender, RoutedEventArgs e)
+        {
+            // Egy adott napon hiányzók kiszűrése
+            int nap = Convert.ToInt32(tbxNap.Text);
+            var eredm = adatok
+                .Where(x => x.Elso <= nap && x.Utolso >= nap)
+                .Select(x => x.Nev)
+                .ToArray();
+
+            tbxFeladat5.Clear();
+            if (eredm.Length > 0)
+                foreach (var item in eredm)
+                {
+                    tbxFeladat5.Text += item.ToString() + "\n";
+                }
+        }
+
+        private void btnFeladat6_Click(object sender, RoutedEventArgs e)
+        {
+            var eredm = adatok.Sum(x => x.Utolso) - adatok.Sum(x => x.Elso);
+
+
+            tbxFeladat6.Clear();
+            tbxFeladat6.Text = eredm.ToString();
+            //foreach (var item in eredm)
+            //{
+            //    tbxFeladat6.Text += item.ToString() + "\n";
+            //}
         }
     }
 }
