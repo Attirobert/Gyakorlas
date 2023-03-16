83fc1ee84344668ecfcf8fb8f2713cf96b045579
Attirobert <karobatti@gmail.com>
2023-03-16 11:13:52 +0100

    Hello World teszt OK.

---
+++ HelloWorldTests/HelloWorldPrTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace HelloWorldTests
+{
+    [TestClass]
+    public class HelloWorldPrTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            string vartEredm = "Hello World!",
+                kapottEredm = string.Empty;
+
+            // Act
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                HelloWorld.HelloWorldPr.Main();
+                kapottEredm = sw.ToString().Trim();
+            }
+
+            // Assert
+            Assert.AreEqual(vartEredm, kapottEredm);
+        }
+    }
+}
---
+++ HelloWorldTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorldTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloWorldTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9003ac86-2868-40c2-a14b-e5943989e461")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
