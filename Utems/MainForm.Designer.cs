d15b446dd1bd1ee394e24452fc0db2a0dcaf514c
Attila Róbert Kovács <kovacs.attila.robert@dszcberegszaszi.hu>
2023-03-19 18:34:03 +0100

    Utems még hibás

---
+++ Utems/MainForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace Utems
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnStop);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 384);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 66);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(93, 21);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(578, 21);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormMain";
+            this.Text = "Bemutató óra 11/A";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Utems/MainForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Utems
+{
+    // Adatszerkezet megadása
+    struct Utem
+    {
+        public DateTime Idopont;    // Mikor kell jelezni
+        public string Szoveg;       // Mit kell kiírni
+    }
+
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        // Osztályváltozók
+        private string file = @"..\..\adatok.csv";
+        private List<Utem> utems = new List<Utem>();
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            #region Ütemezés beolvasása
+            string[] darabol;
+            Utem utem;
+            DateTime dt = DateTime.Today;   // Napi dátum
+
+            using (StreamReader sr = new StreamReader(file, Encoding.UTF8))
+            {
+                sr.ReadLine();  // Az első sor fejléc - eldobjuk
+
+                while (!sr.EndOfStream)
+                {
+                    darabol = sr.ReadLine().Split(';');
+                    dt.AddHours(Convert.ToInt32(darabol[0]));
+                    dt.AddMinutes(Convert.ToInt32(darabol[1]));
+                    utem.Idopont = dt;
+                    utem.Szoveg = darabol[2];
+
+                    utems.Add(utem);
+                }
+
+            }
+            #endregion Ütemezés beolvasása
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (utems[0].Idopont <= DateTime.Now)
+            {
+                MessageBox.Show(utems[0].Szoveg);
+                utems.Remove(utems[0]);
+                if (utems.Count == 0) timer1.Enabled = false;
+            }
+        }
+    }
+}
