c2f154985d413b767f2dff8782ef25ca0981c13b
Attirobert <karobatti@gmail.com>
2023-10-03 14:46:29 +0200

    Iktató MySql próbálkozás

---
+++ Iktato/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Iktato
+{
+    public partial class Form1 : Form
+    {
+        Data.Connection con=new Data.Connection();
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text= String.Empty;
+            label1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Data.Connection.dataSource();
+                con.connOpen();
+                label1.Text = "Sikeres kapcsolódás!";
+
+            }
+            catch (Exception) {
+                label1.Text = "Sikertelen kapcsolódás!";
+
+            }
+            finally
+            {
+                con.connClose();
+                label1.Visible = true;
+           }
+        }
+    }
+}
